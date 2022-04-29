85b6a326aaa37fdc6fe3eb376987150b26607d18
Denis Cherniavskii <denis.cherniavskii@t-systems.com>
2022-04-29 12:32:05 +0300

    LVL-03: Improve invert object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,8 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (obj == null) {
-    return undefined;
+  if (!obj) {
+    return;
   }
 
   return Object.fromEntries(Object.entries(obj)
